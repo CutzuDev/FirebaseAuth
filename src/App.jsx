5f31fc32535a8a7ac50245e9e48142d53fcac4e4
CutzuDev <alexfarkas05@gmail.com>
2022-10-06 22:03:16 +0300

    Finally fixed the log out error

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,67 @@
-import { auth } from "./firebase/init.js";
+import { auth, db } from "./firebase/init.js";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  doc,
+  query,
+  where,
+} from "firebase/firestore";
 import { useState } from "react";
 import Nav from "./components/Nav";
 import Inputs from "./components/Inputs";
 import Logged from "./components/Logged";
+import { async } from "@firebase/util";
+import _ from "underscore";
 
 function App() {
   const [user, setUser] = useState({});
   const [logged, setLogged] = useState(false);
 
-  onAuthStateChanged(auth, (user, logged) => {
-    if (user && logged) {
-      setUser(user);
-      setLogged(true);
+  function createPost() {
+    const post = {
+      title: "Finish Firebase",
+      description: "I could never snitch",
+      uid: user.uid,
+    };
+    addDoc(collection(db, "posts"), post);
+  }
+
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, "posts"));
+    const posts = docs.map((elem) => ({ ...elem.data(), id: elem.id }));
+    console.log(posts);
+  }
+
+  async function getPostById() {
+    const hardcodedId = "AmOHW9tdHZIs7Vz0XyEL";
+    const postRef = doc(db, "posts", hardcodedId);
+    const postSnap = await getDoc(postRef);
+    if (postSnap.exists()) {
+      const post = postSnap.data();
+      console.log(post);
+    } else {
+      console.log("Failed");
     }
+  }
+
+  onAuthStateChanged(auth, (user) => {
+    setTimeout(() => {
+      if (user) {
+        setUser(user);
+        setLogged(true);
+      } else {
+        setUser({});
+        setLogged(false);
+      }
+    }, 10);
   });
 
   function registerUser(auth, email, password) {
@@ -39,7 +82,6 @@ function App() {
       .then(({ user }) => {
         setUser(user);
         setLogged(true);
-
         alert("Succes! te pupa tata");
       })
       .catch((e) => {
@@ -53,9 +95,9 @@ function App() {
   }
 
   function logoutUser() {
+    signOut(auth);
     setLogged(false);
     setUser({});
-    signOut(auth);
   }
 
   return (
@@ -66,6 +108,18 @@ function App() {
       ) : (
         <Inputs auth={auth} registerUser={registerUser} loginUser={loginUser} />
       )}
+
+      <div className="container--full">
+        <button className="btn hover" onClick={createPost}>
+          Make Post
+        </button>
+        <button className="btn hover" onClick={getAllPosts}>
+          Get All Posts
+        </button>
+        <button className="btn hover" onClick={getPostById}>
+          Get Post by ID
+        </button>
+      </div>
     </div>
   );
 }
